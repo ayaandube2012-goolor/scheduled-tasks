557969e5d4cda1348240056e21fbe6af350d4665
ayaandube2012-goolor <ayaandube2012@gmail.com>
2026-06-14 20:50:44 +0100

    Refactor birthday email sending logic with pandas

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+##################### Extra Hard Starting Project ######################
+import pandas
 import datetime as dt
-import smtplib
 import random
-import os
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+import smtplib
+
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
 
 now = dt.datetime.now()
-day = now.weekday()
-if day == 0:
-    with open("quotes.txt", "r") as quotes:
-        quotes_str = quotes.read()
-        quotes_list = quotes_str.splitlines()
-    quote = random.choice(quotes_list)
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs="24AyaanDube@lgs.slough.sch.uk",
-                            msg = "Subject: Good morning\n"
-                            f"From: {MY_EMAIL}\n"
-                            "To: 24AyaanDube@lgs.slough.sch.uk\n"
+month = now.month
+day = now.day
+df = pandas.read_csv("birthdays.csv")
+for i,r in df.iterrows():
+    if r["month"] == month and r["day"] == day:
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+        with open(f"letter_templates/letter_{random.randint(1, 4)}.txt", 'r') as letter:
+            bday_letter = letter.read()
+            bday_letter = bday_letter.replace("[NAME]", r["name"])
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+        my_email = "goolor.love.isthebest@gmail.com"
+        my_password = "xpup otrj smmu ujtx"
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=r["email"],
+                                msg="Subject: Happy Birthday\n"
+                            f"From: {my_email}\n"
+                            f"To: {r["email"]}\n"
                             "\n"
-                            "Hello,\n"
-                            "I know this is probably the day you hate the most so here's some motivation:\n"
-                            f"{quote}")
-        print("email sent")
+                            f"{bday_letter}")
